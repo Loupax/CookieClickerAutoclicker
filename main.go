9033d6f58986e0d6abf0ff1eb0717a0aa152a2d9
Kostas Loupasakis <loupax@gmail.com>
2018-04-21 15:54:21 +0300

    Can't believe I'm still doing this

--- main.go
+++ main.go
@@ -1,22 +1,23 @@
 package main
 
 import (
-	"github.com/go-vgo/robotgo"
-	"regexp"
 	"time"
+	"regexp"
+	"github.com/go-vgo/robotgo"
 )
 
 func main() {
 	for {
-		match, _ := regexp.MatchString("Cookie Clicker$", robotgo.GetTitle())
-		if match == true {
-			x, y := robotgo.GetMousePos()
-			if x >= 122 && x <= 300 && y >= 311 && y <= 472 {
-				robotgo.MouseClick()
-				time.Sleep(time.Millisecond)
+		select {
+		case <-time.After(time.Millisecond):
+			match, _ := regexp.MatchString("Cookie Clicker$", robotgo.GetTitle())
+			if match == true {
+				x, y := robotgo.GetMousePos()
+				if x >= 122 && x <= 300 && y >= 311 && y <= 472 {
+					robotgo.MouseClick()
+					time.Sleep(time.Millisecond)
+				}
 			}
-		} else {
-			time.Sleep(time.Second * 5)
 		}
 	}
 }
